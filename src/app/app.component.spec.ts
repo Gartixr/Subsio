264670d27faa677644df613f9c3ebd0c73c75413
Gartixr <adidumitrica@gmail.com>
2025-05-06 18:15:41 +0200

    Remove app from app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Subsio' title`, () => {
+  it(`should have the 'subsio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Subsio');
+    expect(app.title).toEqual('subsio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Subsio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, subsio');
   });
 });
